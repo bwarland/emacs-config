ea282a79c1de33326ff21487fa16613b3cfe9ef0
Bjorn Warland <bwarland@gmail.com>
2021-01-22 21:23:57 +0000

    org-capture treningstabell

--- reg.el
+++ reg.el
@@ -1,7 +1,6 @@
 (cond ((eq system-type 'gnu/linux) (set-register ?c '(file . "~/GitHub/emacs-config/custom.el")))
       ((eq system-type 'windows-nt) (set-register ?c '(file . "~/C:/Users/bjorwa/Documents/GitHub/emacs-config/custom.el"))))
 
-;; (set-register ?C '(file . "~/notebook/col.org"))
 (set-register ?d '(file . "~/GitHub/Journal/dagbok.org"))
 (set-register ?g '(file . "~/GitHub/"))
 (set-register ?f '(file . "~/GitHub/Notater/ferdigheter.org"))
@@ -9,5 +8,6 @@
 (set-register ?j '(file . "~/GitHub/Markedsanalyse/journaler/journal.org"))
 (set-register ?l '(file . "~/GitHub/Markedsanalyse/journaler/liq.org"))
 (set-register ?r '(file . "~/GitHub/Markedsanalyse/journaler/reg.org"))
+(set-register ?t '(file . "~/GitHub/Journal/dagbok.org"))
 (set-register ?e '(file . "~/.emacs"))
 
