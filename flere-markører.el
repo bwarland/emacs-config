7bac032bd41e644bbc5de02aec27661828faf6b1
Bjorn Warland <bwarland@gmail.com>
2021-03-20 10:00:05 +0000

    org-babel

--- flere-markører.el
+++ flere-markører.el
@@ -1,11 +1,11 @@
-    (use-package multiple-cursors
-      :defer t
-      :bind
-      (
-       ("C-c m t" . mc/mark-all-like-this)
-       ("C-c m l" . mc/edit-lines)
-       ("C-c m e" . mc/edit-end-of-lines)
-       ("C-c m a" . mc/edit-beginning-of-lines)
-       ("C-c m n" . mc/edit-next-line-like-this)
-       ("C-c m p" . mc/edit-previous-like-this)
-       ("C-c m r" . set-rectangular-region-anchor)))
+(use-package multiple-cursors
+  :defer t
+  :bind
+  (
+   ("C-c m t" . mc/mark-all-like-this)
+   ("C-c m l" . mc/edit-lines)
+   ("C-c m e" . mc/edit-end-of-lines)
+   ("C-c m a" . mc/edit-beginning-of-lines)
+   ("C-c m n" . mc/edit-next-line-like-this)
+   ("C-c m p" . mc/edit-previous-like-this)
+   ("C-c m r" . set-rectangular-region-anchor)))
--- helm-konf.el
+++ helm-konf.el
@@ -1,40 +1,40 @@
-  (use-package helm
-    :ensure t
-    :bind (
-           ;; ("M-x" . helm-m-x)
-           ("C-x c-f" . helm-find-files)
-           ("C-x f" . helm-recentf)
-           ("M-y" . helm-show-kill-ring)
-           ("C-x b" . helm-buffers-list)	;
-           ("C-x c y" . helm-yas-complete)
-           ("C-x c y" . helm-yas-create-snippet-on-region)
-           ("C-c h o" . helm-occur)
-           ("C-c h x" . helm-register)
-           ("C-c <f9>" . helm-calcul-expression)
-           ("C-c <f1>" . helm-colors)
-           ("M-p" . helm-previous-line)
-           ("M-k" . helm-next-line)
-           ("M-i" . helm-previous-page)
-           ("M-k" . helm-next-page)
-           ("M-h" . helm-beginning-of-buffer)
-           ("M-h" . helm-end-of-buffer)
-           )
-    :init
-    (setq helm-buffers-lazy-matching t
-          helm-recentf-fuzzy-match t
-          helm-locate-fuzzy-match t
-          helm-split-window-in-side-p t
-          helm-lisp-fuzzy-completion t
-          helm-autoresize-max-height 0
-          helm-autoresize-min-height 20)
-    :config
-    (ido-mode -1)
-    (helm-mode 1)
-    (helm-autoresize-mode 1))
-  (when (eq system-type 'gnu/linux) (use-package helm-ls-git
-                                      :after helm
-                                      :bind (("C-x c-d" . helm-browse-project)
-                                             ("C-x r p" . helm-projects-history))))
+(use-package helm
+  :ensure t
+  :bind (
+         ;; ("M-x" . helm-m-x)
+         ("C-x c-f" . helm-find-files)
+         ("C-x f" . helm-recentf)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x b" . helm-buffers-list)	;
+         ("C-x c y" . helm-yas-complete)
+         ("C-x c y" . helm-yas-create-snippet-on-region)
+         ("C-c h o" . helm-occur)
+         ("C-c h x" . helm-register)
+         ("C-c <f9>" . helm-calcul-expression)
+         ("C-c <f1>" . helm-colors)
+         ("M-p" . helm-previous-line)
+         ("M-k" . helm-next-line)
+         ("M-i" . helm-previous-page)
+         ("M-k" . helm-next-page)
+         ("M-h" . helm-beginning-of-buffer)
+         ("M-h" . helm-end-of-buffer)
+         )
+  :init
+  (setq helm-buffers-lazy-matching t
+        helm-recentf-fuzzy-match t
+        helm-locate-fuzzy-match t
+        helm-split-window-in-side-p t
+        helm-lisp-fuzzy-completion t
+        helm-autoresize-max-height 0
+        helm-autoresize-min-height 20)
+  :config
+  (ido-mode -1)
+  (helm-mode 1)
+  (helm-autoresize-mode 1))
+(when (eq system-type 'gnu/linux) (use-package helm-ls-git
+                                    :after helm
+                                    :bind (("C-x c-d" . helm-browse-project)
+                                           ("C-x r p" . helm-projects-history))))
 
 (use-package swiper-helm
   :ensure t
