5e40de3a6473289d90f2bd0dee76fa506a55386d
Bjorn Warland <bwarland@gmail.com>
2021-04-21 15:06:09 +0100

    kbell->win

--- orgtodo.el
+++ orgtodo.el
@@ -1,11 +1,12 @@
-(setq org-todo-fast-todo-selection t
-        org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "DATA(D)" "PRES(p)" "BAL(b)" "FIN(f)" "PRAK(P)" "|" "DONE(d)"))
+  (setq org-todo-fast-todo-selection t
+        org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w@/!)" "|" "DONE(d)" "CANC(c@)"))
         org-todo-keyword-faces (quote (("TODO" :foreground "red" :background "white")
-                                       ("FIN" :foreground "red" :background "white")
+                                       ;; ("FIN" :foreground "red" :background "white")   ;; utgår
                                        ("NEXT" :foreground "blue" :background "white")
-                                       ("PRAK" :foreground "blue" :background "white")
-                                       ("DATA" :foreground "light slate grey" :background "white" :weight "bold")
-                                       ("PRES" :foreground "dark turquoise" :background "white" :weight "bold")
-                                       ("BAL" :foreground "dark olive green" :background "white" :weight "bold")
+                                       ;; ("PRAK" :foreground "blue" :background "white") ;; utgår
+                                       ("WAIT" :foreground "light slate grey" :background "white" :weight "bold") ;;utgår
+                                       ;; ("PRES" :foreground "dark turquoise" :background "white" :weight "bold")   ;;utgår
+                                       ;; ("BAL" :foreground "dark olive green" :background "white" :weight "bold")  ;;utgår
+                                       ("CANC" :foreground "dark olive green" :background "white" :weight "bold")  ;;utgår
                                        ("DONE" :foreground "dark green" :background "white" :weight "bold"))))
 (setq org-log-done 'note)
