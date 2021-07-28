8f42bbeade6e4a0573a226a1a3ea2a9595d3d150
Bjorn Warland <bwarland@gmail.com>
2021-07-28 19:41:24 +0100

    mbell->kbell/nvaar

--- programmering.el
+++ programmering.el
@@ -1,12 +1,12 @@
-;; (setq python-shell-completion-native-enable nil)
-(setq org-src-preserve-indentation t)
-(setq python-shell-interpreter "/home/bwarland/anaconda3/bin/python3")
+    ;; (setq python-shell-completion-native-enable nil)
+  (setq org-src-preserve-indentation t)
+  (setq python-shell-interpreter "/home/bwarland/anaconda3/bin/python3")
 
-(use-package racket-mode
-    :defer t
-    :config
-    (when (eq system-type 'windows-nt) ((setq racket-racket-program "c:\\program files\\racket\\racket.exe"
-                                              racket-raco-program "c:\\program files\\racket\\raco.exe"))))
-;; (require 'ob-racket)
+    (use-package racket-mode
+      :defer t
+      :config
+      (when (eq system-type 'windows-nt) ((setq racket-racket-program "c:\\program files\\racket\\racket.exe"
+                                                racket-raco-program "c:\\program files\\racket\\raco.exe"))))
+  ;; (require 'ob-racket)
 
-(setq sql-sqlite-program "/home/bwarland/anaconda3/bin/sqlite3")
+  (setq sql-sqlite-program "/home/bwarland/anaconda3/bin/sqlite3")
