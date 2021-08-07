9071cb13a9e59519e2468c491f9f857a1b405f86
Bjorn Warland <bwarland@gmail.com>
2021-08-07 11:47:32 +0100

    company

--- agenda-files.el
+++ agenda-files.el
@@ -1,27 +1,27 @@
-  ;; (cond ((eq system-type 'windows-nt) 
-  ;;        (setq mjo "~/:c/users/bjorwa/documents/GitHub/Markedsanalyse/journaler/"
-  ;;              not "~/:c/users/bjorwa/documents/GitHub/Markedsanalyse/notater/"))
-  ;;       ((eq system-type 'gnu/linux) 
-  ;;        (setq mjo "~/GitHub/Markedsanalyse/journaler/"
-  ;;              not "~/GitHub/Markedsanalyse/notater/")))
-  ;; (setq org-agenda-files (concat mjo "energi-master.org"))
-  ;; ;; (setq org-agenda-files (concat mjo "balanser.org"))
-  ;;                         (concat mjo "presentasjoner.org")
-  ;;                         (concat mjo "liq.org")
-  ;;                        (concat mjo "bd.org")
-  ;;                         (concat mjo "pk.org")
-  ;;                         (concat mjo "journal.org")
-  ;;                         (concat not "informatikk.org")
-  ;;                         (concat not "ferdigheter.org"))
+;; (cond ((eq system-type 'windows-nt) 
+;;        (setq mjo "~/:c/users/bjorwa/documents/GitHub/Markedsanalyse/journaler/"
+;;              not "~/:c/users/bjorwa/documents/GitHub/Markedsanalyse/notater/"))
+;;       ((eq system-type 'gnu/linux) 
+;;        (setq mjo "~/GitHub/Markedsanalyse/journaler/"
+;;              not "~/GitHub/Markedsanalyse/notater/")))
+;; (setq org-agenda-files (concat mjo "energi-master.org"))
+;; ;; (setq org-agenda-files (concat mjo "balanser.org"))
+;;                         (concat mjo "presentasjoner.org")
+;;                         (concat mjo "liq.org")
+;;                        (concat mjo "bd.org")
+;;                         (concat mjo "pk.org")
+;;                         (concat mjo "journal.org")
+;;                         (concat not "informatikk.org")
+;;                         (concat not "ferdigheter.org"))
 
-  ;; (setq org-agenda-files '("~/GitHub/Markedsanalyse/journaler/energi-master.org"
-  ;;                           "~/GitHub/Markedsanalyse/journaler/balanse.org"
-  ;;                           "~/GitHub/Markedsanalyse/journaler/presentasjoner.org"
-  ;;                           "~/GitHub/Markedsanalyse/journaler/liq.org"
-  ;;                           "~/GitHub/Markedsanalyse/journaler/bd.org"
-  ;;                           "~/GitHub/Markedsanalyse/journaler/pk.org"
-  ;;                           "~/GitHub/notater/samfunndrill.org"
-  ;;                           "~/GitHub/Markedsanalyse/journaler/journal.org" 
-  ;;                           "~/GitHub/notater/moeter.org" 
-  ;;                           "/home/bwarland/GitHub/notater/informatikk.org"
-  ;;                           "~/GitHub/notater/ferdigheter.org"))
+;; (setq org-agenda-files '("~/GitHub/Markedsanalyse/journaler/energi-master.org"
+;;                           "~/GitHub/Markedsanalyse/journaler/balanse.org"
+;;                           "~/GitHub/Markedsanalyse/journaler/presentasjoner.org"
+;;                           "~/GitHub/Markedsanalyse/journaler/liq.org"
+;;                           "~/GitHub/Markedsanalyse/journaler/bd.org"
+;;                           "~/GitHub/Markedsanalyse/journaler/pk.org"
+;;                           "~/GitHub/notater/samfunndrill.org"
+;;                           "~/GitHub/Markedsanalyse/journaler/journal.org" 
+;;                           "~/GitHub/notater/moeter.org" 
+;;                           "/home/bwarland/GitHub/notater/informatikk.org"
+;;                           "~/GitHub/notater/ferdigheter.org"))
--- org-roam.el
+++ org-roam.el
@@ -5,12 +5,12 @@
   :custom
   (org-roam-directory "~/GitHub/Notater/org-roam")
   :bind (("C-c n l" . org-roam-buffer-toggle)
-	 ("C-c n f" . org-roam-node-find)
-	 ("C-c n i" . org-roam-node-insert)
-	 ("C-c n g" . org-roam-node-graph)
-	 ("C-c n c" . org-roam-capture)
-	 )"C-c b t" . org-roam-buffer-toggle)
-	 )
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n g" . org-roam-node-graph)
+         ("C-c n c" . org-roam-capture)
+         ;; )"C-c b t" . org-roam-buffer-toggle)
+         )
   :config
   (org-roam-setup))
 (require 'org-roam-protocol)
