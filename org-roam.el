0d47b32ca1b176264ba33a00813b0411ba83057f
Bjorn Warland <bwarland@gmail.com>
2021-09-05 17:57:34 +0100

    mbell->sky

--- org-roam.el
+++ org-roam.el
@@ -4,7 +4,8 @@
     (setq org-roam-v2-ack t)
     :custom
     (org-roam-directory "~/GitHub/Notater/org-roam")
-    (org-roam-dailies-directory "~/GitHub/Notater/daglig/")
+    (org-roam-completion-everywhere t)
+    ;; (org-roam-dailies-directory "~/GitHub/Notater/daglig/")
     :bind (("C-c n l" . org-roam-buffer-toggle)
            ("C-c n f" . org-roam-node-find)
            ("C-c n i" . org-roam-node-insert)
@@ -12,7 +13,9 @@
            ("C-c n c" . org-roam-capture)
            ("C-c b t" . org-roam-buffer-toggle)
 	   ("C-c d s" . org-roam-db-sync)
-	   ("C-c n j" . org-roam-capture-today)
+	   ("C-c n j" . org-roam-dailies-capture-today)
+	   :bind
+	   ("C-M-i" . completion-at-point)
            )
     :config
     (org-roam-setup)
