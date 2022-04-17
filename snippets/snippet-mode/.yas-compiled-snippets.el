82640962d4b7a659dc6f5b74d401e80eff51013b
Bjorn Warland <bwarland@gmail.com>
2022-04-17 13:34:22 +0100

    mbell>snippets/corr

--- snippets/snippet-mode/.yas-compiled-snippets.el
+++ snippets/snippet-mode/.yas-compiled-snippets.el
@@ -1,8 +1,2 @@
 ;;; Compiled snippets and support files for `snippet-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'snippet-mode
-		     '((">tr" "[[file:/home/bw/Documents/Marked/Turkey/rapporter/${1:filnavn}.pdf][${2:rapportnavn}]$0" "Turkey" nil nil nil "/home/bw/GitHub/emacs-config/snippets/snippet-mode/turkey" nil nil)))
-
-
-;;; Do not edit! File generated at Sun Apr 17 13:21:58 2022
+;;; Do not edit! File generated at Sun Apr 17 13:32:56 2022
