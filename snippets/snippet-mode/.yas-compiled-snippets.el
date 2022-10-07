0114ecc90ce2d7f68ba0647e2c7298f3bb810f78
Bjorn Warland <bwarland@gmail.com>
2022-10-07 06:21:54 +0100

    biba>kbell:snippets

--- snippets/snippet-mode/.yas-compiled-snippets.el
+++ snippets/snippet-mode/.yas-compiled-snippets.el
@@ -1,2 +1,6 @@
 ;;; Compiled snippets and support files for `snippet-mode'
+<<<<<<< HEAD
 ;;; Do not edit! File generated at Fri Sep 30 11:23:36 2022
+=======
+;;; Do not edit! File generated at Wed Oct  5 09:12:32 2022
+>>>>>>> 68e9331 (biba>kbell:snippets)
