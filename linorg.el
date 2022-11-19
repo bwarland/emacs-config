8d17a98f599df2f0475f94ef9c5498e9fb436135
Bjorn Warland <bwarland@gmail.com>
2022-11-19 14:24:31 +0000

    vet ikke helt

--- linorg.el
+++ linorg.el
@@ -1,38 +1,38 @@
-   (use-package org
-     :mode (("\\.org$" . org-mode))
-     :hook (org-mode . org-bullets-mode)
-     :bind
-     (("C-c o" . 'org-mode)
-      ("C-c c" . 'org-capture)
-      ("<f5>" . 'org-copy-subtree)
-      ("C-c a" . 'org-agenda)
-      ("C-c l" . 'org-store-link)
-      ("C-c C-." . org-time-stamp)
-      ("C-c C-t". 'org-todo)		;
-      ("C-c t" . 'org-show-todo-tree)
-      ("C-c C-w" . 'org-capture-refile)
-      ("C-c C-k" . 'org-capture-kill))
-     :config
-     (setq initial-major-mode 'org-mode
-   	  org-hide-leading-stars t
-   	  org-startup-indented t
-   	  org-startup-folded t
-   	  org-pretty-entities t
-   	  org-pretty-entities-include-sub-superscripts t
-   	  org-treat-s-cursor-todo-selection-as-state-change nil
-   	  org-directory "~/GitHub"
-   	  org-reverse-note-order nil
-   	  org-refile-use-outline-path t
-   	  org-outline-path-complete-in-steps nil
-   	  org-refile-allow-creating-parent-nodes (quote confirm)
-   	  org-indirect-buffer-display 'current-window
-   	  org-hide-emphasis-markers t
-   	  org-tags-column -50
-   	  org-ellipsis " ..."
-   		    ))
+(use-package org
+   :mode (("\\.org$" . org-mode))
+   :hook (org-mode . org-bullets-mode)
+   :bind
+   (("C-c o" . 'org-mode)
+    ("C-c c" . 'org-capture)
+    ("<f5>" . 'org-copy-subtree)
+    ("C-c a" . 'org-agenda)
+    ("C-c l" . 'org-store-link)
+    ("C-c C-." . org-time-stamp)
+    ("C-c C-t". 'org-todo)		;
+    ("C-c t" . 'org-show-todo-tree)
+    ("C-c C-w" . 'org-capture-refile)
+    ("C-c C-k" . 'org-capture-kill))
+   :config
+   (setq initial-major-mode 'org-mode
+   	org-hide-leading-stars t
+   	org-startup-indented t
+   	org-startup-folded t
+   	org-pretty-entities t
+   	org-pretty-entities-include-sub-superscripts t
+   	org-treat-s-cursor-todo-selection-as-state-change nil
+   	org-directory "~/GitHub"
+   	org-reverse-note-order nil
+   	org-refile-use-outline-path t
+   	org-outline-path-complete-in-steps nil
+   	org-refile-allow-creating-parent-nodes (quote confirm)
+   	org-indirect-buffer-display 'current-window
+   	org-hide-emphasis-markers t
+   	org-tags-column -50
+   	org-ellipsis " ..."
+   		  ))
 
-  (setq org-id-track-globally t)
-  (setq org-agenda-files (directory-files-recursively "~/GitHub/Notater/journaler/" "\\.org$"))
+(setq org-id-track-globally t)
+(setq org-agenda-files (directory-files-recursively "~/GitHub/Notater/journaler/" "\\.org$"))
 
 (cond ((eq system-type 'windows-nt) (setq default-directory "~/c:users/bjorwa/documents/GitHub/"))
       ((eq system-type 'gnu/linux) (setq default-directory "~/GitHub/")))
@@ -117,20 +117,20 @@
 	       "%?\n%c ")
                      )))
 
-  (org-babel-do-load-languages
+(org-babel-do-load-languages
    'org-babel-load-languages (quote ((emacs-lisp . t)
                                  ;;    (racket . t)
-				     ;; (guile . t)
+                                     ;; (guile . t)
                                      (sqlite . t)
                                      (python . t)
-				     (shell . t)
+                                     (shell . t)
                                      ;; (ipython .t)
-				     ;; (ditaa .t)
+                                     ;; (ditaa .t)
                                      )))
 ;; (setq ob-ditaa-jar-path "/usr/bin/ditaa")
-(add-to-list 'load-path "/home/bw/.emacs.d/quelpa/recipes/ob-diagrams")
-(load "ob-diagrams.el")
-(require 'ob-diagrams)
+;; (add-to-list 'load-path "/home/bw/.emacs.d/quelpa/recipes/ob-diagrams")
+;; (load "ob-diagrams.el")
+;; (require 'ob-diagrams)
 (setq org-babel-python-command "python3")
 
 ;; (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
