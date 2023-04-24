bcb25268642c9851570120f892759ae66fe2a45b
Bjørn Warland <bwarland@gmail.com>
2023-04-24 19:35:00 +0100

    redningsaksjon

--- snippets/snippet-mode/.yas-compiled-snippets.el
+++
@@ -1,6 +0,0 @@
-;;; Compiled snippets and support files for `snippet-mode'
-<<<<<<< HEAD
-;;; Do not edit! File generated at Thu Mar 23 08:08:38 2023
-=======
-;;; Do not edit! File generated at Fri Apr 14 10:11:05 2023
->>>>>>> 36ee818 (nye yasnippets for land)
