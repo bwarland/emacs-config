f419d125a6bb7be4666c7d5cdc07ff1e8a7bc574
Bjorn Warland <bwarland@gmail.com>
2023-09-26 20:09:59 +0100

    endrer dailies

--- snippets/inferior-python-mode/.yas-compiled-snippets.el
+++ snippets/inferior-python-mode/.yas-compiled-snippets.el
@@ -1,2 +1,2 @@
 ;;; Compiled snippets and support files for `inferior-python-mode'
-;;; Do not edit! File generated at Wed Aug 23 09:45:25 2023
+;;; Do not edit! File generated at Tue Sep 26 20:09:24 2023
--- snippets/python-mode/.yas-compiled-snippets.el
+++ snippets/python-mode/.yas-compiled-snippets.el
@@ -5,4 +5,4 @@
 		     '(("naming" "" "naming" nil nil nil "/home/bw/GitHub/emacs-config/snippets/python-mode/naming" nil nil)))
 
 
-;;; Do not edit! File generated at Wed Aug 23 09:45:25 2023
+;;; Do not edit! File generated at Tue Sep 26 20:09:24 2023
